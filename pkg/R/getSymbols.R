05552772ee8eafc6f89ac693b1fb88e78021d445
edunov (Sergey Edunov) <edunov@gmail.com>
2014-01-11 23:00:10 +0000

    Fixing most commong errors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rusquant@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getSymbols.R
+++ pkg/R/getSymbols.R
@@ -18,7 +18,7 @@ function(Symbols,env,return.class='xts',index.class='Date',
      default.to <- to
 
      if(missing(verbose)) verbose <- FALSE
-     if(missing(auto.assign)) auto.assign <- TRUE
+     if(missing(auto.assign)) auto.assign <- FALSE
 
      p <- 0
 
@@ -116,7 +116,7 @@ function(Symbols,env,return.class='xts',index.class='Date',
                              sep='.')
        }
 
-
+   
        fr <- convert.time.series(fr=fr,return.class=return.class)
        if(is.xts(fr) && p>7)
          indexClass(fr) <- index.class
@@ -137,6 +137,110 @@ function(Symbols,env,return.class='xts',index.class='Date',
 }
 
 
+"getSymbols.mfd" <-
+function(Symbols,env,return.class='xts',index.class='Date',
+         from='2007-01-01',
+         to=Sys.Date(),
+         adjust=FALSE,
+         period='day',
+         ...)
+{
+     importDefaults("getSymbols.mfd")
+     this.env <- environment()
+     for(var in names(list(...))) {
+        # import all named elements that are NON formals
+        assign(var, list(...)[[var]], this.env)
+     }
+
+     default.return.class <- return.class
+     default.from <- from
+     default.to <- to
+
+     if(missing(verbose)) verbose <- FALSE
+     if(missing(auto.assign)) auto.assign <- FALSE
+
+     p <- 0
+
+	 if ("tick" == period) p <- 0
+     if ("1min" == period) p <- 1
+     if ("5min" == period) p <- 2
+     if ("10min" == period) p <- 3
+     if ("15min" == period) p <- 4
+     if ("30min" == period) p <- 5
+     if ("hour" == period) p <- 6
+     if ("day" == period) p <- 7
+     if ("week" == period) p <- 8
+     if ("month" == period) p <- 9
+
+     if (p==0) {
+        message(paste("Unkown period ", period))
+     }
+    for(i in 1:length(Symbols)) {
+   
+	format(as.Date(from),"%d.%m.%Y")->mfd.from
+	format(as.Date(to),"%d.%m.%Y")->mfd.to
+	Symbols.name <- getSymbolLookup()[[Symbols[[i]]]]$name
+    Symbols.name <- ifelse(is.null(Symbols.name),Symbols[[i]],Symbols.name)
+	 data("tickers")
+	 SYMBOL.GROUP<- tickers[which(tickers[,4]==Symbols),1]
+	 SYMBOL.ID<-tickers[which(tickers[,4]==Symbols),3]
+	 
+       if (length(SYMBOL.ID)==0){
+           if (verbose)
+                cat("Don't know about",Symbols[[i]],"\n\n")
+           next
+       }
+
+	 
+	 mfd.URL<-"http://mfd.ru/export/handler.ashx/Data.txt?"
+	 
+	 stock.URL <- paste(mfd.URL,
+	 "TickerGroup=",SYMBOL.GROUP,
+	 "&Tickers=",SYMBOL.ID,
+	"&Alias=false&Period=",p,
+	"&timeframeValue=1&timeframeDatePart=day&StartDate=",mfd.from,"&EndDate=",mfd.to,
+	"&SaveFormat=0&SaveMode=0&FileName=Date18112013_23112013.txt&FieldSeparator=%253b&DecimalSeparator=.&DateFormat=yyyyMMdd&TimeFormat=HHmmss&DateFormatCustom=&TimeFormatCustom=&AddHeader=true&RecordFormat=0&Fill=false",sep="")
+	 tmp <- tempfile()
+     download.file(stock.URL, destfile=tmp,quiet=TRUE)
+     fr <- read.table(tmp, sep=";",header=TRUE)
+       unlink(tmp)	 	 
+	 if (p>=7) {
+	 
+	 
+	 paste("0",as.character(fr[fr[,4]<100000 & fr[,4]>=10000,4]),sep="")->fr[fr[,4]<100000 & fr[,4]>=10000,4]
+	 paste("00",(fr[as.double(fr[,4])<10000 & as.double(fr[,4])>0,4]),sep="")->fr[as.double(fr[,4])<10000 & as.double(fr[,4])>0,4]
+	 paste("00000",(fr[fr[,4]=='0',4]),sep="")->fr[fr[,4]=='0',4]						
+     fr <- xts(apply(as.matrix(fr[,(5:10)]),2, as.numeric), as.POSIXct(strptime(paste(fr[,3],fr[,4]), "%Y%m%d %H%M%S")),src='mfd',updated=Sys.time())
+	             colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
+                             c('Open','High','Low','Close','Volume','OPEN_INTEREST'),
+                             sep='.')
+							 }
+	 if (p<7) {
+            
+fr <- xts(apply(as.matrix(fr[,(5:10)]),2, as.numeric), as.Date(strptime(fr[,3], "%Y%m%d")),src='mfd',updated=Sys.time())
+	             colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
+                             c('Open','High','Low','Close','Volume','OPEN_INTEREST'),
+                             sep='.')}
+	
+       fr <- convert.time.series(fr=fr,return.class=return.class)
+       if(is.xts(fr) && p>7)
+         indexClass(fr) <- index.class
+
+       Symbols[[i]] <-toupper(gsub('\\^','',Symbols[[i]]))
+       if(auto.assign)
+         assign(Symbols[[i]],fr,env)
+       if(i >= 5 && length(Symbols) > 5) {
+         message("pausing 1 second between requests for more than 5 symbols")
+         Sys.sleep(1)
+       }
+	 }  
+     if(auto.assign)
+       return(Symbols)
+	 
+	 if(exists('fr'))  
+		return(fr)
+}
+
 "loadStockList" <-
 function (verbose = FALSE){
     stocklist.URL = 'http://www.finam.ru/cache/icharts/icharts.js'
@@ -147,13 +251,24 @@ function (verbose = FALSE){
     ids <- sub("var .*?= \\[", "", fr[1])
     ids <- sub("\\];", "", ids)
     ids <- strsplit(ids, ",")
+	
+	markets <- sub("var .*?= \\[", "", fr[4])
+    markets <- sub("\\];", "", markets)
+    markets <- strsplit(markets, ",")
 
     names <- sub("var .*?= \\[", "", fr[3])
     names <- sub("\\];", "", names)
     names <- gsub("'", "", names)
     names <- strsplit(names, ",")
+	names[[1]]->names
+	names[-(8497)]->names
     res <- unlist(ids)
-    names(res) <- unlist(names)
+	
+	data<-data.frame(names,res,markets)
+	data[data[,3]!=3,]->data
+	rbind(data[(data[,1]%in%data[data[,3]==1,1]) & data[,3]==1,],data[!(data[,1]%in%data[data[,3]==1,1]),])->data
+	data[,2]->res
+    names(res) <- data[,1]
     return(res)
 }
 
@@ -226,7 +341,7 @@ function(Symbols,env,return.class='xts',index.class='Date',
      default.to <- to
 
      if(missing(verbose)) verbose <- FALSE
-     if(missing(auto.assign)) auto.assign <- TRUE
+     if(missing(auto.assign)) auto.assign <- FALSE
 
      forts.URL <- "http://www.rts.ru/ru/forts/contractresults-exp.html?"
 
