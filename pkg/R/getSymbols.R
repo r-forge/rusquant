87953e68d6a6a107d03d2451cf3cd2702e7ad43f
arbuzov1989 (Vyacheslav Arbuzov) <arbuzov1989@gmail.com>
2015-03-15 06:44:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rusquant@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getSymbols.R
+++ pkg/R/getSymbols.R
@@ -375,7 +375,7 @@ function (verbose = FALSE){
     names <- gsub("'", "", names)
     names <- strsplit(names, ",")
 	names[[1]]->names
-    names[12008]<-''
+    names[12009]<-''
     res <- unlist(ids)
 	
 	data<-data.frame(names,res,markets)
