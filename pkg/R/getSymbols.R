d7270f1b7b8e1e3b9e00aea7fa90af1e277bc6fc
arbuzov1989 (Vyacheslav Arbuzov) <arbuzov1989@gmail.com>
2016-07-20 16:08:01 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rusquant@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/getSymbols.R
+++ pkg/R/getSymbols.R
@@ -1,3 +1,236 @@
+#function retrives data from poloniex through its public API
+"getSymbols.Poloniex" <- function #S3 function (Poloniex is a class of first argument)
+(Symbols,env,return.class='xts',index.class='Date',
+ from='2007-01-01',
+ to=Sys.Date(),
+ adjust=FALSE,
+ period='day',
+ ...)
+{
+	importDefaults("getSymbols.Poloniex"); #rewrite default values if specified by setDefaults
+	local_env <- environment()
+	for(var in names(list(...))) {
+		# import all named elements that are NON formals
+		assign(var, list(...)[[var]], local_env)
+	}
+
+	options(warn = -1)
+	default.return.class <- return.class
+	default.from <- from
+	default.to <- to
+
+	if(missing(verbose)) verbose <- TRUE
+	if(missing(auto.assign)) auto.assign <- FALSE
+	
+	Polo.period <- 0
+	Polo.max_candles <- 1000
+	Polo.from <- as.numeric(as.POSIXct(from)) #convert to UNIX timestamp
+	Polo.to <- as.numeric(as.POSIXct(to)) #convert to UNIX timestamp
+
+	Polo.downloadUrl <- 'https://poloniex.com/public?command=returnChartData'
+
+	switch(period, #select candle period according to data frequency
+		 '5min' = {
+		   Polo.period <- 300
+		 },
+		 '15min' = {
+		   Polo.period <- 900
+		 },
+		 '30min' = {
+		   Polo.period <- 1800
+		 },
+		 '2hours' = {
+		   Polo.period <- 7200
+		 },
+		 '4hours' = {
+		   Polo.period <- 14400
+		 },
+		 'day' = {
+		   Polo.period <- 86400
+		 }
+	)
+
+	#example API usage
+	#"https://poloniex.com/public?command=returnChartData&currencyPair=USDT_BTC&period=7200&start=1468711470&end=1468757470"
+	
+	for(i in 1:length(Symbols)) {
+		Polo.url <- paste(Polo.downloadUrl,
+                        "&currencyPair=", Symbols[[i]],
+                        "&period=", Polo.period,
+                        "&start=", Polo.from,
+                        "&end=", Polo.to, sep="")
+		tmp <- tempfile()
+		download.file(Polo.url, destfile = tmp, quiet = TRUE) #get JSON object
+		rawdata <- readLines(tmp) #read raw data from file
+		if(substr(rawdata, 3, 7) == 'error') {
+		  
+		  stop(paste('Error!', substr(rawdata, 11, nchar(rawdata) - 2)))
+		}
+		
+		#parsing JSON object
+		str_obs <- strsplit(rawdata, split = "},{", fixed = TRUE)
+		str_obs[[1]][1] <- substr(str_obs[[1]][1], 3, nchar(str_obs[[1]][1]))
+		temp <- str_obs[[1]][length(str_obs[[1]])]
+		substr(temp, 1, nchar(temp)-2) -> str_obs[[1]][length(str_obs[[1]])]
+		
+		ticker_header <- c('date', 'high', 'low', 'open', 'close', 'volume')
+		ticker_length <- length(ticker_header)
+		lst <- list()
+		
+		for(j in 1:length(str_obs[[1]])) {
+		  str <- str_obs[[1]][j]
+		  str_par <- strsplit(str, ",", fixed = TRUE)
+		  vec_row <- c()
+		  for(k in 1:ticker_length){
+		    vec_row <- append(vec_row, values = substr(str_par[[1]][k], 4+nchar(ticker_header[k]), nchar(str_par[[1]][k])))
+		  }
+		  
+		  lst[[length(lst)+1]] <- vec_row
+		}
+		
+		res <- do.call(rbind.data.frame, lst)
+		names(res) <- ticker_header
+		
+		nts <- xts(apply(res[,2:length(res)], 2, as.numeric),
+		           as.POSIXct(as.numeric(as.character(res[, 1])), origin="1970-01-01", tz ="GMT"))
+		
+		fr <- convert.time.series(fr=nts, return.class=return.class)
+		
+		Symbols[[i]] <-toupper(gsub('\\^','',Symbols[[i]]))
+		
+		if(auto.assign){
+		  assign(Symbols[[i]], fr, env)
+		}
+		
+	}
+	
+	if(auto.assign){
+		return(Symbols)
+	}
+
+	return(fr)
+}
+
+#function retrives data from alortrade broker
+"getSymbols.Alor" <- function #S3 function (Alor is a class of first argument)
+(Symbols,env,return.class='xts',index.class='Date',
+ from='2007-01-01',
+ to=Sys.Date(),
+ adjust=FALSE,
+ period='day',
+ ...)
+{
+  importDefaults("getSymbols.Alor"); #rewrite default values if specified by setDefaults
+  local_env <- environment()
+  for(var in names(list(...))) {
+    # import all named elements that are NON formals
+    assign(var, list(...)[[var]], local_env)
+  }
+  
+  options(warn = -1)
+  default.return.class <- return.class
+  default.from <- from
+  default.to <- to
+  
+  if(missing(verbose)) verbose <- TRUE
+  if(missing(auto.assign)) auto.assign <- FALSE
+  board <- "MICEX" #data is grabbed from Moscow Exchange by default
+  
+  Alor.period <- 0
+  Alor.max_candles <- 1000
+  Alor.from <- as.POSIXct(from)
+  Alor.to <- as.POSIXct(to)
+  
+  Alor.downloadUrl <- 'http://history.alor.ru/?'
+  
+  switch(period, #select candle period according to data frequency
+         'tick' = {
+           Alor.period <- 0
+         },
+         '1min' = {
+           Alor.period <- 1
+         },
+         '5min' = {
+           Alor.period <- 5
+         },
+         '10min' = {
+           Alor.period <- 10
+         },
+         '15min' = {
+           Alor.period <- 15
+         },
+         '20min' = {
+           Alor.period <- 20
+         },
+         '30min' = {
+           Alor.period <- 30
+         },
+         'hour' = {
+           Alor.period <- 60
+         },
+         'day' = {
+           Alor.period <- 1440
+         }
+  )
+  
+  #example of request
+  #http://history.alor.ru/?board=MICEX&ticker=LKOH&period=5&from=2016-03-21&to=2016-07-12&bars=5
+  
+  result <- as.null()
+  
+  for(i in 1:length(Symbols)) {
+    #building request to a REST service
+    #bypassing 1000 obs restriction
+    repeat
+    {
+      Alor.url <- paste(Alor.downloadUrl, "board=", board,
+                        "&ticker=", Symbols[[i]],
+                        "&period=", Alor.period,
+                        "&from=", Alor.from,
+                        "&to=", Alor.to,
+                        "&bars=",Alor.max_candles)
+      
+      temp <- tempfile()
+      download.file(Alor.url, destfile = temp, quiet = TRUE) #request to a service
+      #reading result as a table
+      res <- read.table(temp, header = FALSE, col.names = c('Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume'))
+      res$Date <- paste(res$Date, " ", res$Time) #merge time a date columns
+      res[,!(names(res) == 'Time')] #remove time column
+      unlink(temp) #release resource
+      
+      #if responce returns same date break a loop
+      if(!is.null(result)){
+        if(result$Date[1] == res$Date[nrow(res)]){
+          break;
+        }
+      }
+      
+      Alor.to <- as.POSIXct(res$Date[nrow(res)]) + Alor.period * 60 #update a date of observation
+      result <- if(is.null(result)) res[order(res$Date), ] else merge(result, res, all=TRUE)
+      
+      Sys.sleep(0.01) #delay between requests
+    }
+    
+    #build xts time series
+    newts <- xts(apply(result[,3:length(result)], 2, as.numeric) , as.POSIXct(result[, 1]))
+    fr <- convert.time.series(fr=newts,return.class=return.class)
+    
+    Symbols[[i]] <-toupper(gsub('\\^','',Symbols[[i]]))
+    
+    if(auto.assign){
+      assign(Symbols[[i]], fr, env)
+    }
+  }
+  
+  if(auto.assign){
+    return(Symbols)
+  }
+  
+  return(fr)
+}
+
+
+
 "getSymbols.rogov" <-
 function(Symbols,env,return.class='xts',index.class='Date',
          from='2007-01-01',
@@ -106,6 +339,30 @@ for(i in 1:length(Symbols)) {
          Sys.sleep(1)
        }
      }
+	 print('The RogovIndex© indices and tools are proprietary to and distributed by Mikhail Rogov.' )
+	 print('All content of the Rogov Index© is proprietary to Mikhail Rogov Terms and Conditions of Access Provider at our discretion,')
+	 print('provide you with services including, but not restricted to, RogovIndex© indices and tools. ')
+	 print('You agree to comply with the conditions imposed on your use of the services, as set out in these Terms and Conditions of Access and elsewhere in our services. ')
+	 print('These services may be outside our control or provided by a third party in which in case we cannot take responsibility for their content, or for any delays,')
+	 print('interruptions or errors in the provisions of these additional services, provided we have exercised reasonable care and diligence in the selection of such ')
+	 print('providers.Certain data accessible on our services is the intellectual property of us. The data is protected by copyright and other intellectual laws ')
+	 print('and all ownership rights remain with us. You may only use the data retrieved from our services for your own purposes while accessing our services. ')
+	 print('Such use will be in accordance with these Terms and Conditions of Access and the requirements set out elsewhere on our services. You may not copy, distribute')
+	 print('or redistribute the data, including by caching, framing or similar means or sell, resell, re-transmit or otherwise make the data retrieved from our services')
+	 print('available in any manner to any third party.The data is provided "as is." We or any third party shall not be liable to you or any third party for any loss or damage,')
+	 print('direct, indirect or consequential, arising from (i) any inaccuracy or incompleteness in, or delays, interruptions, errors or omissions in the delivery of the data or any ')
+	 print('other information supplied to you through our services or (ii) any decision made or action taken by you or any third party in reliance upon the data. Third party nor we ')
+	 print('shall be liable for loss of business revenues, lost profits or any punitive, indirect, consequential, special or similar damages whatsoever, whether in contract, tort or ')
+	 print('otherwise, even if advised of the possibility of such damages incurred by you or any third party.Where the information consists of pricing or performance data, the data ')
+	 print('contained therein has been obtained from sources believed reliable. Data computations are not guaranteed by any information service provider, third party or us or any affiliates')
+	 print('and may not be complete. Neither any information service provider, third party or us give any warranties, as to the accuracy, adequacy, quality or fitness, timeless, ')
+	 print('non-infringement, title, of any information for a particular purpose or use and all such warranties are expressly excluded to the fullest extent that such warranties ')
+	 print('may be excluded by law. You bear all risk from any use or results of using any information.You are responsible for validating the integrity of any information received ')
+	 print('over the Internet.Transmission may be subject to arbitrary delays beyond our control, which may delay the provision of our services and the execution of your orders.')
+	 print('You acknowledge that neither any information service provider, third party nor we will be liable to you or any third party for any losses arising from such delay. ')
+	 print('In no event will any information provider, third party or we, be liable for any consequential loss including but not limited to special, incidental, direct or indirect ')
+	 print('damages resulting from delay or loss of use of our services. We are not responsible for any damage to your computer, software, modem, telephone or other property resulting ')
+	 print('from your use of our services.')
      if(auto.assign)
        return(Symbols)
 	
@@ -251,6 +508,7 @@ function(Symbols,env,return.class='xts',index.class='Date',
 
 }
 
+
 "getSymbols.mfd" <-
 function(Symbols,env,return.class='xts',index.class='Date',
          from='2007-01-01',
@@ -318,7 +576,7 @@ function(Symbols,env,return.class='xts',index.class='Date',
      download.file(stock.URL, destfile=tmp,quiet=TRUE)
      fr <- read.table(tmp, sep=";",header=TRUE)
        unlink(tmp)	 	 
-	 if (p>=7) {
+	 if (p<7) {
 	 
 	 
 	 paste("0",as.character(fr[fr[,4]<100000 & fr[,4]>=10000,4]),sep="")->fr[fr[,4]<100000 & fr[,4]>=10000,4]
@@ -329,7 +587,7 @@ function(Symbols,env,return.class='xts',index.class='Date',
                              c('Open','High','Low','Close','Volume','OPEN_INTEREST'),
                              sep='.')
 							 }
-	 if (p<7) {
+	 if (p>=7) {
             
 fr <- xts(apply(as.matrix(fr[,(5:10)]),2, as.numeric), as.Date(strptime(fr[,3], "%Y%m%d")),src='mfd',updated=Sys.time())
 	             colnames(fr) <- paste(toupper(gsub('\\^','',Symbols.name)),
@@ -375,11 +633,10 @@ function (verbose = FALSE){
     names <- gsub("'", "", names)
     names <- strsplit(names, ",")
 	names[[1]]->names
+	names[-(11414)]->names
     res <- unlist(ids)
-	markets<-unlist(markets)
 	
-	
-	data<-data.frame(names[1:min(length(res),length(names))],res[1:min(length(res),length(names))],markets[1:min(length(res),length(names))])
+	data<-data.frame(names,res,markets)
 	data[data[,3]!=3,]->data
 	rbind(data[(data[,1]%in%data[data[,3]==1,1]) & data[,3]==1,],data[!(data[,1]%in%data[data[,3]==1,1]),])->data
 	data[,2]->res
